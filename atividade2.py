2e28c92f4abcb166029955a2e3d96587946cb7b8
carloscesar09 <81190518+carloscesar09@users.noreply.github.com>
2021-03-31 18:36:44 -0300

    Create atividade2.py

---
+++ atividade2.py
@@ -0,0 +1,12 @@
+def calcula_soma(lista_de_numeros):
+ lista = [3, 8, 9, 2, 0]
+ soma = 0
+ lista_de_numeros = lista
+ i = 0
+ while lista > i:
+    soma += lista [i]
+    i+= 1
+ return soma 
+
+calcula_soma (input(f"3, 8, 9, 2, 0"))
+print (soma)
