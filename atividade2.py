0fe093c3569bdc8cfc5d32a16ecf8b743188f178
carloscesar09 <81190518+carloscesar09@users.noreply.github.com>
2021-03-31 19:22:00 -0300

    Update atividade2.py

--- atividade2.py
+++ atividade2.py
@@ -10,3 +10,28 @@ def calcula_soma(lista_de_numeros):
 
 calcula_soma (input(f"3, 8, 9, 2, 0"))
 print (soma)
+
+def converte_numeros (lista_de_numeros):
+  lista = [3, 8, 9, 2, ]
+  soma = 0
+  lista_de_numeros = lista
+  i = 0
+    lista.split(int(f" 3, 8, 9, 2,") )
+  return lista
+print (lista) 
+
+
+def processa_numeros(val1, val2):
+ lista = [3, 5]
+ return (val1, val2)
+print (val1, val2)
+
+
+def main (lista):
+ lista = [3, 8,9, 2]
+ soma = 0
+ for num in lista:
+   soma += num
+   media= soma/2 
+print (media)
+
