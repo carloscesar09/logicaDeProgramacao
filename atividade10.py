57442f57d91e270e0844cf1ec2eeb871b6116db4
carloscesar09 <81190518+carloscesar09@users.noreply.github.com>
2021-04-11 18:13:34 -0300

    Create atividade10.py

---
+++ atividade10.py
@@ -0,0 +1,49 @@
+#inicio
+def exercicio1:
+ num_de_entrada = int ( input( ))
+ lista = [ ]
+ i = 0
+ for i in range (num_de_entrada)
+  if i % 5 == 0
+   if i % 2 == 1
+ print ( i )
+#fim 
+
+
+# inicio
+def exercicio2: 
+ num_de_entrada = int (input( )) 
+ lista = [ ]
+for 1 in range ( num_de_entrada)
+lista.apend (input ( ))
+1 += 1
+return lista 
+
+retorno exercício2 ( )
+print ( retorno )
+# fim 
+
+
+# inicio
+def exercicio3: 
+ num_de_entrada = int (input( )) 
+ lista = [ ]
+for 1 in range ( num_de_entrada)
+lista.apend (input ( ))
+  if num_de_entrada > 5
+    print ( num_de_entrada )
+  else:
+    print ( invalido )
+# fim 
+
+# inicio
+def exercicio4:
+ opcao = input( )
+ while opcao != "d"
+  if opcao == "a"
+    print ("PSG")
+  elif opcao == "b"
+    print ("BAYERN")
+ else:
+    print ("invalido")
+# fim
