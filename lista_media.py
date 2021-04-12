3f5abea6d628502ac4ec778e4a68c4d720eca3c3
carloscesar09 <81190518+carloscesar09@users.noreply.github.com>
2021-04-12 20:42:53 -0300

    Create lista_media.py

---
+++ lista_media.py
@@ -0,0 +1,16 @@
+# inicio
+lista_media = ["7", "8", "9", "6", "8", "8" ]
+def funcao_4 ( ): 
+  lista_media = [ ]
+  i = 0 
+  while i < rangee ( len (lista))
+  i += 1
+  elemento = lista [i]
+  for elemento = lista:
+   if elemento >= "7"
+     lista_media += 1
+   if lista_media / len > 0.75
+     print ("Professor padrão")
+   else:
+     print( "Professor coxa")
+# fim
