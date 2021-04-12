3514dfcf36b2182f6855c9a69da1ba0c26c1d1f9
carloscesar09 <81190518+carloscesar09@users.noreply.github.com>
2021-04-12 20:29:35 -0300

    Create funcao1media.py

---
+++ funcao1media.py
@@ -0,0 +1,12 @@
+# inicio
+lista = [8, 8, 6]
+ def funcao1 ( ):
+  n1 = input ( )
+  n2 = input ( )
+  n3 = input ( )
+  n1 = float(input( "primeira nota do aluno: "))
+  n2 = float(input( "segunda nota do aluno: "))
+  n3 = float(input ("terceira nota do aluno: ")) 
+  media = (n1 + n2 + n3 )/3
+   print (media) 
+# fim 
