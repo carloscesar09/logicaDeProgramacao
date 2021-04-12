5998e969f93b22c5b80fad4e2fac9eea93e883b0
carloscesar09 <81190518+carloscesar09@users.noreply.github.com>
2021-04-12 20:31:56 -0300

    Create lista_de_saida.py

---
+++ lista_de_saida.py
@@ -0,0 +1,13 @@
+# inicio
+  lista = input ( )
+  lista = lista.split ( )
+  lista = [ ]
+  def funcao_2 ( ):
+    i= 0
+    lista.apend (input( )) 
+    i += 1
+    return lista
+
+    retorno funcao_2( )
+    print ( retorno )
+# fim 
