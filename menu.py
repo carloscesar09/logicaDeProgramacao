aa5adaec84a31547a9baf011b161641d26f74b68
carloscesar09 <81190518+carloscesar09@users.noreply.github.com>
2021-04-12 20:33:46 -0300

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,11 @@
+# inicio
+def funcao ( ):
+  opcao = input ( )
+  While opcao != "z" or "Z"
+   if opcao == "a"
+     print("globo")
+   elif opcao == "b"
+     print ("sbt")
+   else:
+     print ("invalido")
+# fim 
