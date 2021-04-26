16b07a187366039f05321741d79a6d4b8757de3b
carloscesar09 <81190518+carloscesar09@users.noreply.github.com>
2021-04-26 17:00:51 -0300

    Create atividade12.py

---
+++ atividade12.py
@@ -0,0 +1,28 @@
+#inicio
+def maior_numero(num1, num2, num3)
+ if(num1 > num2 and num1 > num3):
+   print (num1)
+ elif (num2 > num1 and num2> num3):
+   print (num2)
+ elif (num3 > num1 and num3> num2):
+   print (num3)
+#fim 
+
+#inicio
+def converter_string(str)
+  return str.split(0
+#fim
+
+#inicio
+def soma_lista(list)
+  i = 0
+  sum= 0
+  while(i < len(list) ):
+    sum += list[1]
+  return sum
+#fim
+
+#inicio
+def criar_string(list):
+  retourn " ".join(list) 
+# fim
